59e7634a14126a04f6a8f676a448cd2857efe0d1
Seo Hye Won <101081013+hyewonss@users.noreply.github.com>
2023-08-31 20:48:19 +0900

    최종수정전

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ import 'package:myapp/utils.dart';
 import 'package:myapp/page-1/chatting.dart';
 import 'package:myapp/page-1/message.dart';
 import 'package:myapp/page-1/new_message.dart';
-// import 'package:myapp/page-1/.dart';
-// import 'package:myapp/page-1/info.dart';
-// import 'package:myapp/page-1/-pcq.dart';
-// import 'package:myapp/page-1/-ABT.dart';
-// import 'package:myapp/page-1/-TmT.dart';
-// import 'package:myapp/page-1/-2oB.dart';
-// import 'package:myapp/page-1/-8Ad.dart';
-// import 'package:myapp/page-1/-Gsj.dart';
-// import 'package:myapp/page-1/-LbK.dart';
 
 
 void main() async{
--- lib/page-1/chatting.dart
+++ lib/page-1/chatting.dart
@@ -7,6 +7,7 @@ import 'package:myapp/page-1/message.dart';
 import 'package:myapp/page-1/new_message.dart';
 import 'package:myapp/utils.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
 
 
 
@@ -19,8 +20,22 @@ class chatScreen extends StatefulWidget {
 
 class _chatScreenState extends State<chatScreen> {
   @override
+
   void initState(){
     super.initState();
+  }
+  String url = "";
+  String topurl = "assets/page-1/images/puppy.jpg";
+
+  void _successloadchat(){
+    setState(() {
+      url = "assets/page-1/images/successpuppy.jpg";
+    });
+  }
+  void _restloadchat(){
+    setState(() {
+      url = "assets/page-1/images/restpuppy.jpg";
+    });
   }
 
   @override
@@ -30,20 +45,103 @@ class _chatScreenState extends State<chatScreen> {
     double ffem = fem * 0.97;
     return Scaffold(
       appBar:AppBar(
-        title: Text('멍뭉이와의 채팅방'),
+        title: Text('멍뭉의 채팅방'),
         centerTitle: true,
       ) ,
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-                child: Messages()
-            ),
-            NewMessage(),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Column(
+            children: [
+              Flexible(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      // rectangle10CFK (1:273)
+                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 11*fem, 0*fem),
+                      width: 79*fem,
+                      height: 139*fem,
+                      child: Image.asset(
+                        'assets/page-1/images/rectangle-10.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      // autogrouprzzhiDf (EFm3sDS2MxkqwyoDZ7RZzh)
+                      margin: EdgeInsets.fromLTRB(0*fem, 31*fem, 0*fem, 25*fem),
+                      width: 248*fem,
+                      height: double.infinity,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            // Eho (1:274)
+                            left: 0*fem,
+                            top: 0*fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 248*fem,
+                                height: 83*fem,
+                                child: Image.asset(
+                                  'assets/page-1/images/.png',
+                                  width: 248*fem,
+                                  height: 83*fem,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            // Lkq (1:277)
+                            left: 65*fem,
+                            top: 12*fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 150*fem,
+                                height: 68*fem,
+                                child: Text(
+                                  '오늘의 미션은 산책하기 입니다!\n완료하셨나요?',
+                                  style: SafeGoogleFont (
+                                    'Cafe24 Oneprettynight',
+                                    fontSize: 16*ffem,
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.2575*ffem/fem,
+                                    color: const Color(0xff000000),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: _successloadchat,
+                  child: Text(
+                    "멍뭉이에게 보여주기"
+                  ),
+              ),
+              ElevatedButton(
+                onPressed: _restloadchat,
+                child: Text(
+                    "오늘은 쉬어갈랭"
+                ),
+              ),
+              new Image(image: AssetImage(url)),
+              Flexible(
+                child: Container(
+                  child: Messages(),
+                ),
+              ),
+              NewMessage(),
+            ],
+          ),
         ),
       )
     );
   }
+
 }
 
